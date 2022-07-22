63e616e6fdb9e06396d88f9f9acde4aa675e0cc1
Anuar <anuar12102001@gmail.com>
2022-07-22 11:33:22 +0600

    Added Models and UserController. Added appsettings.Production.

---
+++ SairanTicketsAPI/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SairanTicketsAPI.Models;
+
+namespace SairanTicketsAPI.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        public List<User> Users = new List<User>()
+        {
+            new User()
+            {
+                Id = 1,
+                FirstName = "Anuar",
+                LastName = "Bora",
+                Email = "anuar@mail.ru",
+                Password = "anuar123",
+                isDeleted = false
+
+            },
+            new User()
+            {
+                Id = 2,
+                FirstName = "Elvira",
+                LastName = "Nuga",
+                Email = "elvira@mail.ru",
+                Password = "elvira123",
+                isDeleted = false
+
+            },
+            new User()
+            {
+                Id = 3,
+                FirstName = "Diyar",
+                LastName = "Ush",
+                Email = "diyar@mail.ru",
+                Password = "diayr123",
+                isDeleted = false
+
+            },
+
+        };
+
+
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return Users;
+        }
+
+        
+    }
+}
